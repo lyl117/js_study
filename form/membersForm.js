207ee5ff4a625b29a9f976b3bdfcef984634900f
yurimlee <ylee981352@gmail.com>
2021-09-19 14:00:04 +0900

    [#9] form태그 실습

--- form/membersForm.js
+++ form/membersForm.js
@@ -1,3 +1,50 @@
-const testFunction = function(){
-  return false
+const membersGet = sessionStorage.getItem('members');
+const membersLogical = membersGet || '[]';
+const members = JSON.parse(membersLogical);
+
+const membersSubmit = function(form) {
+  const inputTextObject = form['input-text'];
+  try {
+    const evalReturn = eval(inputTextObject.value);
+    console.log(evalReturn);
+  } catch(error) {
+    console.error(error);
+    alert(error);
+    return false;
+  }
 }
+
+const membersCreate = function(member) {
+  members.push(member);
+  membersSet();
+  // window.location.reload();
+  return members;
+};
+
+const membersRead = function() {
+  for(let index in members){
+    document.writeln(members[index]);
+  }
+  return members;
+};
+
+const membersDelete = function(index) {
+  members.splice(index, 1);
+  membersSet();
+  // window.location.reload();
+  return members;
+};
+
+const membersUpdate = function(index, member) {
+  members[index] = member;
+  membersSet();
+  // window.location.reload();
+  return members;
+};
+
+const membersSet = function() {
+  const membersSet = JSON.stringify(members);
+  sessionStorage.setItem('members', membersSet);
+};
+
+membersRead();
