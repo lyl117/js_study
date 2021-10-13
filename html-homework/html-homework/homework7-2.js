7792814f4f8da8f02436559dca462815fbeabc59
yurimlee <ylee981352@gmail.com>
2021-10-13 12:19:23 +0900

    [#6] 숙제

--- html-homework/html-homework/homework7-2.js
+++ html-homework/html-homework/homework7-2.js
@@ -1,16 +1,10 @@
 const copy = function() {
   console.log('copy');
   // 여기에 프로그램을 작성 해주세요.
-  const membersRead = function() {
-    const tagDivParent = document.getElementById('div-parent');
-    const tagDivChild = document.getElementById('input-child');
-    tagDivParent.innerHTML = '';
-    for (let index in members) {
-      const newDivChild = tagDivChild.cloneNode(true);
-      tagDivParent.appendChild(newDivChild);
-    }
-    console.log('Readed', members);
-    return members;
-  };
+  const tagDivParent = document.getElementById('div-parent');
+  const tagDivChilds = document.getElementsByName('input-child');
+  const tagDivChild = tagDivChilds[0];
+  const newDivChild = tagDivChild.cloneNode(true);
+  tagDivParent.appendChild(newDivChild);
   
 }
