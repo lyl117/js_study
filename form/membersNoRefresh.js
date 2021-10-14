8300c1d5934d2e2339c881a23c0d0ade66603807
yurimlee <ylee981352@gmail.com>
2021-10-14 16:27:33 +0900

    [#6] 숙제 - 문제 풀이

--- form/membersNoRefresh.js
+++ form/membersNoRefresh.js
@@ -2,14 +2,24 @@ const membersGet = sessionStorage.getItem('members');
 const membersLogical = membersGet || '[]';
 const members = JSON.parse(membersLogical);
 
-const queryString = new URLSearchParams(window.location.search);
-const nameText = queryString.get('input-text');
+// const queryString = new URLSearchParams(window.location.search);
+// const nameText = queryString.get('input-text');
 
-// const inputTextObjects = document.getElementsByName('input-text');
-// const inputTextObject = inputTextObjects[0];
-const inputTextObject = document.getElementsByName('input-text')[0]; 
+// // const inputTextObjects = document.getElementsByName('input-text');
+// // const inputTextObject = inputTextObjects[0];
+// const inputTextObject = document.getElementsByName('input-text')[0]; 
 
 // inputTextObject.value = nameText;
+
+// const url = new URL(window.location);
+// const queryString = url.searchParams;
+// const nameText = queryString.get('member-name');
+// const nameText = queryString.get('member-age');
+const queryString = new URLSearchParams(window.location.search);
+const nameText = queryString.get('member-name','member-age');
+const inputTextObject = document.getElementsByName('member-name');
+const inputTextObjects = document.getElementsByName('member-age');
+const inputText = inputTextObject[0];
 
 
 // const inputHiddens = queryString.getAll('input-hidden');
