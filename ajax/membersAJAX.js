6386485e8c64667f616b30684b15bed1d9c7c75e
yurimlee <ylee981352@gmail.com>
2021-10-20 12:51:54 +0900

    [#14] 서버와의 통신 update

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -167,22 +167,32 @@ const membersDelete = function(index) {
 //   return members;
 // };
 const membersUpdate = function(index) {
-  const memberNameObject = document.getElementsByName('members-name')[index];
-  const memberAgeObject = document.getElementsByName('members-age')[index];
-  console.log(memberNameObject.value);
-  const name = memberNameObject.value;
-  const age = memberAgeObject.value;
-  // members[index] = {
-  //   name: memberNameObject.value,
-  //   age: memberAgeObject.value
-  // }
-  members[index] = {
+  const url = 'http://localhost:3100/api/v1/members/' + index;
+  const name = document.getElementsByName('members-name')[index].value;
+  const age = document.getElementsByName('members-age')[index].value;
+  const member = {
     name: name,
     age: age
   };
-  membersSet();
-  return membersRead();
+  const xhrObject = new XMLHttpRequest();
+  xhrObject.onreadystatechange = function () {
+    if (xhrObject.readyState !== 4) return;
+    if (xhrObject.status === 200) {
+      membersRead();
+    } else {
+      const error = {
+        status: xhrObject.status,
+        statusText: xhrObject.statusText,
+        responseText: xhrObject.responseText
+      }
+      console.error(error);
+    }
+  };
+  xhrObject.open('PATCH', url);
+  xhrObject.setRequestHeader('Content-Type', 'application/json');
+  xhrObject.send(JSON.stringify(member));
 };
+
 
 
 const membersSet = function() {
