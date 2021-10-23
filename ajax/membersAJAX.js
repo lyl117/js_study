9986e46ca998fffd897d39c4eaabdcf71c639ce4
yurimlee <ylee981352@gmail.com>
2021-10-23 15:59:19 +0900

    [#14] 공통부분 리팩토링 delete

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -129,23 +129,7 @@ const membersRead = function() {
 
 const membersDelete = function(index) {
   const url = 'http://localhost:3100/api/v1/members/' + index;
-  const xhrObject = new XMLHttpRequest();
-  xhrObject.onreadystatechange = function () {
-    if (xhrObject.readyState !== 4) return;
-    if (xhrObject.status === 200) {
-      membersRead();
-    } else {
-      const error = {
-        status: xhrObject.status,
-        statusText: xhrObject.statusText,
-        responseText: xhrObject.responseText
-      }
-      console.error(error);
-    }
-  };
-  xhrObject.open('DELETE', url);
-  xhrObject.setRequestHeader('Content-Type', 'application/json');
-  xhrObject.send();
+  ajax('DELETE', url, undefined, membersRead);
 };
 
 
