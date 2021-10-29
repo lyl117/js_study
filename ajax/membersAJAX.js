e5bc4098391daa24a0c7d1ab06fb91d10710f472
yurimlee <ylee981352@gmail.com>
2021-10-29 12:29:26 +0900

    [#15] axios update 변경

--- ajax/membersAJAX.js
+++ ajax/membersAJAX.js
@@ -154,7 +154,8 @@ const membersUpdate = function(index) {
     name: name,
     age: age
   };
-  ajax('PATCH', url, JSON.stringify(member), membersRead);
+  // ajax('PATCH', url, JSON.stringify(member), membersRead);
+  axios.patch(url, member).then(membersRead);
 };
 
 
